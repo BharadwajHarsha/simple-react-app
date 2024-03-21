131d1196c95d4ad703695c264a260bbbad66bac2
BharadwajHarsha <bharadwajharsha@gmail.com>
2024-03-22 00:17:11 +0530

    change logo and text

--- src/App.js
+++ src/App.js
@@ -7,16 +7,14 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         This is a simple/sample app to try amplify deployment
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h4>Items to bring</h4>
+        <ol>
+          <li>Coffee</li>
+          <li>Tea</li>
+          <li>Milk</li>
+        </ol>
       </header>
     </div>
   );
